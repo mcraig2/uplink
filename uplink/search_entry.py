9a924c46b80c5fd3f4eb0c14fbf9ba188ce6bc38
Mike Craig <mike.craig@uber.com>
2019-07-02 12:56:20 -0400

    Format the output

--- uplink/search_entry.py
+++ uplink/search_entry.py
@@ -7,9 +7,18 @@ from .dbio import load_db
 from .grep_scorer import score
 
 
+def format_entry(entry):
+    """ Take an entry and format it for output """
+    separator = '-' * 80
+    return """
+{separator}
+{entry}
+{separator}""".format(separator=separator, entry=describe_entry(entry))
+
+
 def search_entry(query, conn):
     db = load_db(conn=conn)
     scores = score(db, query)
     most_relevant = db.iloc[scores.idxmax(), :]['entry_id']
 
-    print(describe_entry(most_relevant))
+    print(format_entry(most_relevant))
